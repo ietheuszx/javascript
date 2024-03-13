48bdf41d5bcf36e56548a3ea971e850957ffcc4a
ietheuszx <fernandomaranss68@gmail.com>
2024-03-13 16:40:07 -0400

    questão 02 concruída

--- questao02.js
+++ questao02.js
@@ -1,5 +1,7 @@
 /*
 Questão 2: Como você pode verificar o tipo de uma variável em JavaScript?
 */
-var minhaVariavel = 1234567890987654321;
-console.log(typeof minhaVariavel );
+
+let nome = "SESCNAC"
+
+console.log(typeof nome);
