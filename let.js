474b67d305533e1cccf9d26c82dbf7c22776cf7d
ietheuszx <fernandomaranss68@gmail.com>
2024-03-13 16:23:32 -0400

    questão 01 concluída

--- let.js
+++ let.js
@@ -1,16 +1,14 @@
 // Declaração de uma variável
-let nome; // Escopo de bloco
+//let nome; // Escopo de bloco
 var idade; // Escopo global
 const PI = 3.14; // Escopo de bloco e global
 
-
 function soma() {
   if(true) {
-    //const PI = 3.14; // Escopo global
-    console.log(PI)
+    var nome = "Marans"
+    console.log(nome)
   }
-  PI = 50
-  console.log(PI)
+  nome = "José Londa"
+  console.log(nome)
 }
-
 soma()
