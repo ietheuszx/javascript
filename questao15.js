9f1e1de9b9a03c74b86aeff11a4359321e759151
ietheuszx <fernandomaranss68@gmail.com>
2024-03-25 14:00:54 -0400

    questao15 resolvida

---
+++ questao15.js
@@ -0,0 +1,8 @@
+/*
+Questão 15: Como você pode obter o comprimento de uma string em 
+JavaScript?
+*/
+
+let mensagem = "Olá, mundo!";
+let tamanho = mensagem.length;
+console.log(tamanho); // Caracteres 11
